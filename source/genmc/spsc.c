d0a31fa5e9590e6e2eb6d118cbeda7c48212f4e4
KurtWu10 <rihui.wu@student.unsw.edu.au>
2025-04-14 17:22:28 +1000

    add genmc

---
+++ source/genmc/spsc.c
@@ -0,0 +1,59 @@
+#include <assert.h>
+#include <stdatomic.h>
+#include <threads.h>
+
+#define CAPACITY 1
+
+typedef struct {
+    atomic_int tail;
+    atomic_int head;
+    int buffers[CAPACITY];
+} Queue;
+
+int enqueue(Queue * queue, int buffer) {
+    const int tail_val = atomic_load_explicit(&queue->tail, memory_order_relaxed);
+    const int head_val = atomic_load_explicit(&queue->head, memory_order_acquire);
+
+    if (tail_val - head_val == CAPACITY) {
+        return -1;
+    } else {
+        queue->buffers[tail_val % CAPACITY] = buffer;
+        atomic_store_explicit(&queue->tail, tail_val + 1, memory_order_release);
+        return 0;
+    }
+}
+
+int dequeue(Queue * queue) {
+    const int head_val = atomic_load_explicit(&queue->head, memory_order_relaxed);
+    const int tail_val = atomic_load_explicit(&queue->tail, memory_order_acquire);
+
+    if (tail_val - head_val == 0) {
+        return -1;
+    } else {
+        int buffer = queue->buffers[head_val % CAPACITY];
+        atomic_store_explicit(&queue->head, head_val + 1, memory_order_release);
+        return buffer;
+    }
+}
+
+int forked_thread(void * queuep) {
+    Queue * queue = (Queue *) queuep;
+    enqueue(queue, 42);
+    return 0;
+}
+
+int main() {
+    Queue queue = {0};
+
+    thrd_t thread;
+    if (thrd_create(&thread, forked_thread, (void *) &queue) != thrd_success) {
+        return 1;
+    }
+
+    int ret = dequeue(&queue);
+    assert(ret == -1 || ret == 42);
+    
+    thrd_join(thread, NULL);
+
+    return 0;
+}
